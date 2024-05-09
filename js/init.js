687549c517a9deb80777dc2614b9e3a3e891768f
Scott Miller <scott.miller@hogarthww.com>
2024-05-09 21:43:46 +0100

    intro adjustments

--- js/init.js
+++ js/init.js
@@ -109,7 +109,7 @@ function start(){
     // show
 }
 
-var introTL = gsap.timeline({paused:true,oncComplete:function(){
+var introTL = gsap.timeline({paused:true,defaults:{duration:0,ease:"none"},oncComplete:function(){
     container.addEventListener('click', bindButtons);
     window.addEventListener('keydown', bindButtons);
     mobileControls.addEventListener('touchend', bindButtons);
@@ -125,57 +125,56 @@ function playIntro() {
     // console.log(introLogoWidth);
     introTL.addLabel("reset", 0)
 
-        .to("#container", 0, {className:"noMouse"}, "reset")
-        .to("#intro-primitai",0,{width:introLogoWidth+"px"},"reset")
-        .to("#intro_txt",0,{alpha:0}, "reset")
-        .to("#intro_txt",0,{className:"txt intro-played"}, "reset")
+        .to("#container", {className:"noMouse"}, "reset")
+        .to("#intro-primitai",{width:introLogoWidth+"px"},"reset")
+        .to(["#intro_txt","#glitch"],{display:"none"}, "reset")
         .to(".game-canvas",{className:"game-canvas"},"reset")
         .to(["#bgOverlay-canvas","#bg-canvas"],{className:"hidden"},"reset")
 
     .addLabel("intro-screen", ">")
         .call(typeCodes,["intro_screen_txt",2], "intro-screen")
 
-        .to("#intro-text-dir", 0,{alpha:1}, ">")
-        .to(".intro-text-dir-lines",0,{alpha:1,stagger:0.03},">1")
-        .to("#intro-text",0,{alpha:0,y:100},"<")
+        .to("#intro-text-dir", {alpha:1}, ">")
+        .to(".intro-text-dir-lines", {alpha:1,stagger:0.03},">1")
+        .to("#intro-text", {alpha:0,y:100},"<")
         .call(clearCode,[],">")
-        .to("#intro-text",0,{alpha:1},"<.5")
+        .to("#intro-text", {alpha:1},"<.5")
         .call(typeCodes,["intro_screen_txt2",2],"<1")
-        .to("#intro-text",0,{alpha:0,y:0},"<2")
-        .to("#intro-text-dir",0,{alpha:0},"<")
+        .to("#intro-text", {alpha:0,y:0},"<2")
+        .to("#intro-text-dir", {alpha:0},"<")
         .call(clearCode,[],">")
 
     .addLabel("intro-primitai", ">.1")
         .call(playMusic, [audio_music], "intro-primitai")
-        .to("#intro-text-playing",0,{alpha:1},"intro-primitai")
-        .to("#intro-text",0,{alpha:1,y:20},"intro-primitai")
+        .to("#intro-text-playing", {alpha:1},"intro-primitai")
+        .to("#intro-text", {alpha:1,y:20},"intro-primitai")
 
-        .to("#intro-primitai",1,{alpha:1}, "intro-primitai")
-        .to("#intro-primitai-bg",0,{y:0}, ">")
+        .to("#intro-primitai", 1,{alpha:1}, "intro-primitai")
+        .to("#intro-primitai-bg", {y:0}, ">")
         .to("#intro-primitai-bg",3.5,{y:"-39%",ease:"linear"}, ">")
-        .to("#intro-presents",0,{alpha:1}, "<1.5")
-        .to("#intro-primitai",0,{alpha:0}, "intro-primitai+=5")
+        .to("#intro-presents", {alpha:1}, "<1.5")
+        .to("#intro-primitai", {alpha:0}, "intro-primitai+=5")
 
     .addLabel("intro-licensed-by", ">.2")
-        .to("#intro-licensed-by",0,{alpha:1}, "intro-licensed-by")
-        .to("#intro-roar",0,{alpha:1,clip:"rect(168px, 296px, 170px, 290px)"}, ">")
+        .to("#intro-licensed-by", {alpha:1}, "intro-licensed-by")
+        .to("#intro-roar", {alpha:1,clip:"rect(168px, 296px, 170px, 290px)"}, ">")
         .to("#intro-roar",1,{clip:"rect(168px, 576px, 170px, 0px)",ease:"expo.in"}, ">")
         .to("#intro-roar",1,{clip:"rect(0px, 576px, 490px, 0px)",ease:"sine.in"}, ">.2")
 
     .addLabel("city", ">1")
         .to(["#intro-licensed-by","#intro-roar","#intro-primitai"],0,{alpha:0}, "city")
-        .to("#intro-city",0,{alpha:1}, "city")    
-        .to("#intro-city-txt1", 0, {alpha:1}, "city")
+        .to("#intro-city" ,{alpha:1}, "city")    
+        .to("#intro-city-txt1", {alpha:1}, "city")
         .call(typeText,[intro_city_txt1,2,0], "city")
-        .to("#intro-city-02",0,{y:30}, "city")    
-        .to("#intro-city-03",0,{y:60}, "city")    
-        .to(["#intro-city-cover"],0,{alpha:1}, "city")
+        .to("#intro-city-02" ,{y:30}, "city")    
+        .to("#intro-city-03" ,{y:60}, "city")    
+        .to(["#intro-city-cover"] ,{alpha:1}, "city")
         .to(["#intro-city-01","#intro-city-02","#intro-city-03"],0,{alpha:1}, "city+=1")
         .to(["#intro-city-cover"],3,{alpha:0,ease:"linear"}, "city+=1")
         .to("#intro-city-02",6,{y:0, ease:"linear"}, "city+=3")
         .to("#intro-city-03",6,{y:0, ease:"linear"}, "city+=3")
         .to("#intro-city-txt1", 1, {alpha:0}, "city+=4")
-        .to("#intro-city-txt2", 0, {alpha:1}, "city+=4")
+        .to("#intro-city-txt2", {alpha:1}, "city+=4")
     .addLabel("city2", "city+=3")
         .call(typeText,[intro_city_txt2,2,0], "city2")
         .to("#intro-city-txt2", 1, {alpha:0}, "city2+=3")
@@ -183,9 +182,9 @@ function playIntro() {
 
 
     .addLabel("lockup", ">")
-        .to(["#introContainer"],0,{alpha:1},"lockup")
+        .to(["#introContainer"], {alpha:1},"lockup")
 
-        .to(["#intro-shredded"],0,{scale:0.01,ease:"linear"},"lockup")
+        .to(["#intro-shredded"], {scale:0.01,ease:"linear"},"lockup")
         
 
 
@@ -197,14 +196,14 @@ function playIntro() {
 
 
         .call(clearCode,[],">")
-        .to("#intro-text-playing",0,{alpha:0},">")
-        .to("#intro-text",0,{alpha:1,y:0},">")
+        .to("#intro-text-playing", {alpha:0},">")
+        .to("#intro-text", {alpha:1,y:0},">")
         .call(typeCodes,["intro_screen_txt3",2], ">")
 
         .to("#intro-bg",12,{scale:1.1,ease:"linear",transformOrigin:"center bottom"},"<")
-        .to(["#intro-player-walk"],0,{alpha:1},"<")
+        .to(["#intro-player-walk"], {alpha:1},"<")
         .call(introPlayerWalk,[],"<")
-        .to("#container",0,{className:""},">")
+        .to("#container", {className:""},">")
 
     .addLabel("complete", ">")
         .call(function(){
